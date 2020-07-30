2f24f5d7794938d21166173ddf33b24811c08913
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2020-07-30 15:35:28 +0300

    Remove hardcoded times

--- domain-functions.js
+++ domain-functions.js
@@ -2,4 +2,4 @@ const getAwareness = state => clamp(state.mentalRecovery * state.energy / (MAX_E
 const willFindBottle = state => () => getAwareness(state) > Math.random();
 const getTimeOfDay = state => state.time % units.days(1);
 const getWeekDay = state => Math.floor(state.time / units.days(1)) % 7;
-const isShopOpen = state => isBetween(units.hours(10), units.hours(21), state.time);
+const isShopOpen = state => isBetween(...places.find(p => p.key === PLACE_SHOP).availability, getTimeOfDay(state));
